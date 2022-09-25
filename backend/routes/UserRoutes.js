5a2e1e21b3d582d76e2bc9c5af2f06c163204df9
Ahmad_Muzaki_SA <ahmadmuzaki.s.a@gmail.com>
2022-09-26 03:01:33 +0700

    Update and delete user

--- backend/routes/UserRoutes.js
+++ backend/routes/UserRoutes.js
@@ -1,8 +1,10 @@
 import express from 'express';
-import { createUser, getUserById, getUsers } from '../controller/UserController.js';
+import { createUser, deleteUser, getUserById, getUsers, updateUser } from '../controller/UserController.js';
 
 export const routerUser = express.Router()
 
 routerUser.get('/mahasiswa', getUsers)
 routerUser.get('/mahasiswa/:id', getUserById)
 routerUser.post('/mahasiswa', createUser)
+routerUser.patch('/mahasiswa/:id', updateUser)
+routerUser.delete('/mahasiswa/:id', deleteUser)
